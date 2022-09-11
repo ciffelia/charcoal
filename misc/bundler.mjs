4ce9805cf8be1f90b7b2d908111f6871daa52856
Ciffelia <mc.prince.0203@gmail.com>
2022-09-11 18:02:04 +0900

    build: update bundler

--- misc/bundler.mjs
+++ misc/bundler.mjs
@@ -1,14 +1,45 @@
-/**
- * @type {import('esbuild').BuildOptions}
- */
-export const baseOptions = {
-  logLevel: 'info',
-  color: true,
-  entryPoints: ['./src/index.ts'],
-  outdir: './dist',
-  tsconfig: './tsconfig.build.json',
-  bundle: true,
-  platform: 'neutral',
-  external: ['../../node_modules/*'],
-  sourcemap: true,
+#!/usr/bin/env zx
+
+import { argv } from 'zx'
+
+const esbuild = require('esbuild')
+
+const main = async () => {
+  const format =
+    argv.format === 'esm' || argv.format === 'cjs' ? argv.format : undefined
+  if (format === undefined) {
+    console.error(`Unknown format: ${argv.format}`)
+    process.exit(1)
+  }
+
+  const isAnalyzeMode = argv.analyze === true
+
+  const { metafile } = await esbuild.build({
+    logLevel: 'info',
+    color: true,
+
+    entryPoints: ['./src/index.ts'],
+    outdir: './dist',
+    outExtension: {
+      '.js': format === 'esm' ? '.mjs' : '.cjs',
+    },
+    tsconfig: './tsconfig.build.json',
+    format,
+
+    bundle: true,
+    platform: 'neutral',
+    external: ['../../node_modules/*'],
+    sourcemap: true,
+
+    minify: isAnalyzeMode,
+    metafile: isAnalyzeMode,
+    write: !isAnalyzeMode,
+  })
+
+  if (isAnalyzeMode && metafile !== undefined) {
+    const summary = await esbuild.analyzeMetafile(metafile)
+    console.log(summary)
+  }
 }
+
+main()
